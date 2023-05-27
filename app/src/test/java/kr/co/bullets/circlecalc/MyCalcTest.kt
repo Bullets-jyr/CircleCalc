a298d24b39d83580c6e1817f451fc8a9534eca20
Bullets-jyr <jyr@bullets.co.kr>
2023-05-27 17:01:58 +0900

    145. Our First Local Unit Test

---
+++ app/src/test/java/kr/co/bullets/circlecalc/MyCalcTest.kt
@@ -0,0 +1,27 @@
+package kr.co.bullets.circlecalc
+
+import com.google.common.truth.Truth.assertThat
+import org.junit.Before
+import org.junit.Test
+
+class MyCalcTest {
+    private lateinit var myCalc: MyCalc
+
+    @Before
+    fun setUp() {
+        myCalc = MyCalc()
+    }
+
+    // SubjectUnderTest_actionOrInput_resultState
+    @Test
+    fun calculateCircumference_radiusGiven_returnsCorrectResult() {
+        val result = myCalc.calculateCircumference(2.1)
+        assertThat(result).isEqualTo(13.188)
+    }
+
+    @Test
+    fun calculateCircumference_zeroRadius_returnsCorrectResult() {
+        val result = myCalc.calculateCircumference(0.0)
+        assertThat(result).isEqualTo(0.0)
+    }
+}
